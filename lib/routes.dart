ace43b4f9f7f6622209dfc9d3cd8dfac805a0177
prashantsachan98 <prashantsachan03@gmail.com>
2021-08-30 14:06:44 +0530

    first commit

---
+++ lib/routes.dart
@@ -0,0 +1,10 @@
+import 'dart:js';
+
+import 'package:flutter/widgets.dart';
+import 'package:shop_app/screens/splash/spalsh_screen.dart';
+
+// named routes
+//all our routes will be here
+final Map<String, WidgetBuilder> routes = {
+  SplashScreen.routeName: (context) => SplashScreen(),
+};
